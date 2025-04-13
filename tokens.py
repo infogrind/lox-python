0fbbe331dfec7f616edb09aa985156751bdeb485
Marius Kleiner <kleiner@gmail.com>
2025-04-13 13:50:46 +0200

    Add all Lox's tokens

--- tokens.py
+++ tokens.py
@@ -17,6 +17,87 @@ class RPAREN(Token):
 
 
 @dataclass
+class LBRACE(Token):
+    pass
+
+
+@dataclass
+class RBRACE(Token):
+    pass
+
+
+@dataclass
+class COMMA(Token):
+    pass
+
+
+@dataclass
+class DOT(Token):
+    pass
+
+
+@dataclass
+class MINUS(Token):
+    pass
+
+
+@dataclass
+class PLUS(Token):
+    pass
+
+
+@dataclass
+class SEMICOLON(Token):
+    pass
+
+
+@dataclass
+class SLASH(Token):
+    pass
+
+
+# One or two character tokens
+
+
+@dataclass
+class BANG(Token):
+    pass
+
+
+@dataclass
+class BANG_EQUAL(Token):
+    pass
+
+
+@dataclass
+class EQUAL(Token):
+    pass
+
+
+@dataclass
+class GREATER(Token):
+    pass
+
+
+@dataclass
+class GREATER_EQUAL(Token):
+    pass
+
+
+@dataclass
+class LESS(Token):
+    pass
+
+
+@dataclass
+class LESS_EQUAL(Token):
+    pass
+
+
+# Literals
+
+
+@dataclass
 class IDENT(Token):
     name: str
 
@@ -27,5 +108,83 @@ class STRING(Token):
 
 
 @dataclass
-class EQUAL(Token):
+class NUMBER(Token):
+    pass
+
+
+# Keywords
+
+
+@dataclass
+class AND(Token):
+    pass
+
+
+@dataclass
+class CLASS(Token):
+    pass
+
+
+@dataclass
+class ELSE(Token):
+    pass
+
+
+@dataclass
+class FALSE(Token):
+    pass
+
+
+@dataclass
+class FUN(Token):
+    pass
+
+
+@dataclass
+class FOR(Token):
+    pass
+
+
+@dataclass
+class IF(Token):
+    pass
+
+
+@dataclass
+class NIL(Token):
+    pass
+
+
+@dataclass
+class OR(Token):
+    pass
+
+
+@dataclass
+class PRINT(Token):
+    pass
+
+
+@dataclass
+class RETURN(Token):
+    pass
+
+
+@dataclass
+class SUPER(Token):
+    pass
+
+
+@dataclass
+class TRUE(Token):
+    pass
+
+
+@dataclass
+class VAR(Token):
+    pass
+
+
+@dataclass
+class WHILE(Token):
     pass
