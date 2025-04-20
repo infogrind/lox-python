b1d9b6035fbad252dbb73096f916af19b1f0380e
Marius Kleiner <kleiner@gmail.com>
2025-04-20 17:38:42 +0200

    Added BufferedIterator class to simplify the rest

---
+++ buffered_iterator.py
@@ -0,0 +1,99 @@
+from collections import deque
+from typing import Iterator
+from typing import Deque
+
+
+class BufferedIterator[T]:
+    def __init__(self, iter: Iterator[T], bufsize: int):
+        self._iter = iter
+        self._bufsize = bufsize
+        self._buffer: Deque[T] = deque()
+
+        self._refill_buffer()
+
+    def _refill_buffer(self) -> None:
+        """
+        Loads elements from the iterator until the buffer is at the desired
+        size, or until the end of the input has been reached.
+        """
+        while len(self._buffer) < self._bufsize:
+            try:
+                self._buffer.append(next(self._iter))
+            except StopIteration:
+                return
+
+    def has_next(self) -> bool:
+        """
+        Returns True if there is still an element available to return with next(),
+        False otherwise.
+        """
+        return bool(self._buffer)
+
+    def next(self):
+        """
+        Returns the next element. Raises StopIteration if no more elements are available.
+        """
+        if not self._buffer:
+            raise StopIteration
+
+        result = self._buffer.popleft()
+        self._refill_buffer()
+        return result
+
+    def can_peek(self, pos: int = 0) -> bool:
+        """
+        Returns True if there are enough elements in the buffer to peek at position pos,
+        i.e., if the buffer contains at least pos + 1 elements.
+
+        This should be called before calling peek(), to ensure a peekable element is present.
+        """
+        if pos < 0:
+            raise ValueError(f"Invalid position: {pos}.")
+        if pos >= self._bufsize:
+            raise ValueError(
+                f"In valid position: {pos}. It must be less than the buffer size {self._bufsize}."
+            )
+        return pos < len(self._buffer)
+
+    def peek(self, pos: int = 0) -> T:
+        """
+        Allows to peek at the elements at the head of the iterator. Raises
+        StopIteration if there are not enough elements in the buffer. The
+        optional parameter pos must be a valid index into the buffer, i.e., it
+        must be smaller than bufsize.
+        """
+        if pos < 0:
+            raise ValueError(f"Invalid position: {pos}.")
+        if pos >= self._bufsize:
+            raise ValueError(
+                f"In valid position: {pos}. It must be less than the buffer size {self._bufsize}."
+            )
+        if pos >= len(self._buffer):
+            raise StopIteration
+        return self._buffer[pos]
+
+    def eat(self, val: T) -> bool:
+        """
+        If the character at the head of the iterator is equal to val, advances
+        the iterator (consuming the element) and returns True. Otherwise, returns
+        false and leaves the iterator state unchanged.
+
+        If there are no more elements, eat() also returns False.
+
+        This is an ergonomic improvement. Instead of writing
+          if b.has_next() and b.peek() == "x":
+            b.next()
+            ...
+
+        one can write
+          if b.eat("x"):
+            ...
+        """
+        if not self._buffer:
+            return False
+
+        if self.peek(0) == val:
+            self.next()
+            return True
+
+        return False
---
+++ tests/test_buffered_iterator.py
@@ -0,0 +1,69 @@
+from collections.abc import Buffer
+import unittest
+from buffered_iterator import BufferedIterator
+
+
+class TestBufferedIterator(unittest.TestCase):
+    def test_empty_iterator_has_no_next(self):
+        b = BufferedIterator(iter([]), 1)
+        self.assertFalse(b.has_next())
+
+    def test_empty_iterator_cannot_peek(self):
+        b = BufferedIterator(iter([]), 1)
+        self.assertFalse(b.can_peek())
+        self.assertFalse(b.can_peek(0))
+
+    def test_index_greater_than_bufsize(self):
+        b = BufferedIterator(iter([]), 1)
+        with self.assertRaises(ValueError):
+            b.can_peek(1)
+
+        b = BufferedIterator(iter([]), 2)
+        with self.assertRaises(ValueError):
+            b.can_peek(2)
+
+    def test_advance_through_single_element_buffer(self):
+        b = BufferedIterator(iter([1, 2, 3]), 1)
+        self.assertTrue(b.has_next())
+        self.assertEqual(b.next(), 1)
+
+        self.assertTrue(b.has_next())
+        self.assertEqual(b.next(), 2)
+
+        self.assertTrue(b.has_next())
+        self.assertEqual(b.next(), 3)
+
+        self.assertFalse(b.has_next())
+
+    def test_advance_through_multi_element_buffer(self):
+        b = BufferedIterator(iter([1, 2, 3]), 10)
+        self.assertTrue(b.has_next())
+        self.assertEqual(b.next(), 1)
+
+        self.assertTrue(b.has_next())
+        self.assertEqual(b.next(), 2)
+
+        self.assertTrue(b.has_next())
+        self.assertEqual(b.next(), 3)
+
+        self.assertFalse(b.has_next())
+
+    def test_peek_and_eat(self):
+        b = BufferedIterator(iter([1, 2, 3]), 2)
+
+        self.assertTrue(b.can_peek(0))
+        self.assertEqual(b.peek(0), 1)
+
+        self.assertTrue(b.can_peek(1))
+        self.assertEqual(b.peek(1), 2)
+
+        self.assertTrue(b.eat(1))
+        self.assertTrue(b.eat(2))
+        self.assertFalse(b.eat(4))
+
+        with self.assertRaises(StopIteration):
+            b.peek(1)
+
+        self.assertTrue(b.eat(3))
+
+        self.assertFalse(b.eat(4))
