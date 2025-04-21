42936dbc1187837058b76a9743cf0318b199cc85
Marius Kleiner <kleiner@gmail.com>
2025-04-21 07:59:49 +0200

    Tree node subset (mini expr syntax)

---
+++ syntax.py
@@ -0,0 +1,113 @@
+from dataclasses import dataclass
+
+
+class Node:
+    pass
+
+
+class Expression(Node):
+    pass
+
+
+class LiteralExpression(Expression):
+    pass
+
+
+@dataclass
+class Number(LiteralExpression):
+    value: float
+
+
+@dataclass
+class String(LiteralExpression):
+    value: str
+
+
+@dataclass
+class TrueExpr(LiteralExpression):
+    pass
+
+
+@dataclass
+class FalseExpr(LiteralExpression):
+    pass
+
+
+@dataclass
+class Nil(LiteralExpression):
+    pass
+
+
+@dataclass
+class Grouping(Expression):
+    expr: Expression
+
+
+@dataclass
+class Negative(Expression):
+    expr: Expression
+
+
+@dataclass
+class LogicalNot(Expression):
+    expr: Expression
+
+
+@dataclass
+class EqualEqualExpr(Expression):
+    lhs: Expression
+    rhs: Expression
+
+
+@dataclass
+class NotEqualExpr(Expression):
+    lhs: Expression
+    rhs: Expression
+
+
+@dataclass
+class LessThanExpr(Expression):
+    lhs: Expression
+    rhs: Expression
+
+
+@dataclass
+class LessEqualExpr(Expression):
+    lhs: Expression
+    rhs: Expression
+
+
+@dataclass
+class GreaterThanExpr(Expression):
+    lhs: Expression
+    rhs: Expression
+
+
+@dataclass
+class GreaterEqualExpr(Expression):
+    lhs: Expression
+    rhs: Expression
+
+
+@dataclass
+class Add(Expression):
+    lhs: Expression
+    rhs: Expression
+
+
+@dataclass
+class Subtract(Expression):
+    lhs: Expression
+    rhs: Expression
+
+
+@dataclass
+class Mult(Expression):
+    lhs: Expression
+    rhs: Expression
+
+
+@dataclass
+class Div(Expression):
+    lhs: Expression
+    rhs: Expression
